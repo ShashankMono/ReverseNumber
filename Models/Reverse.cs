2e694252ab31a73f3450ae86474ff7078273adac
shashankg361 <shashankg361@gmail.com>
2024-09-25 19:49:02 +0530

    First-commit

---
+++ Models/Reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber.Models
+{
+    internal class Reverse
+    {
+        public string NumberReverse(int number) {
+
+            string result = "";
+            while (number > 0) {
+                int temp = number % 10;
+                result += temp.ToString();
+                number /= 10;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using ReverseNumber.Models;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Switches();
+        }
+        public static void Switches()
+        {
+            bool run = true;
+            while (run)
+            {
+                Console.WriteLine($"\n0.Exit\n" +
+                    $"1.Reverse\n");
+                int choise = int.Parse(Console.ReadLine());
+
+                switch (choise)
+                {
+                    case 0:
+                        run = false;
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Enter the number");
+                        int number = int.Parse(Console.ReadLine());
+                        Reverse reverse = new Reverse();
+                        Console.WriteLine($"The reverse of the number {number} is {reverse.NumberReverse(number)}");
+                        break;
+                    default:
+                        Console.WriteLine("Please choose correct choise");
+                        break;
+                }
+               
+            }
+        }
+    }
+}
